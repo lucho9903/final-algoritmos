830863314d7460dc67941e5cfab5c09b160709da
unknown <luisfgn99@gmail.com>
2023-11-13 13:21:04 -0500

    correccion de espacio

--- parcial.py
+++ parcial.py
@@ -20,6 +20,7 @@ color_boton = "gray15"
 ancho_boton = 30
 alto_boton = 4
 
+
 boton_programa1 = tk.Button(ventana, text="calculadora basica", bg=color_boton,width=ancho_boton,height=alto_boton, fg="white",command=abrir_programa1)
 boton_programa2 = tk.Button(ventana, text="calculadora cientifica",bg=color_boton,width=ancho_boton,height=alto_boton, fg="white", command=abrir_programa2)
 boton_programa3 = tk.Button(ventana, text="calculadora matrices",bg=color_boton,width=ancho_boton,height=alto_boton, fg="white", command=abrir_programa3)
